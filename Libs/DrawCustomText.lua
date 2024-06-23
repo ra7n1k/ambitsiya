08bdb32efcd778bedf514c376ce7d423fa0a7528
ra7n1k <111421305+ra7n1k@users.noreply.github.com>
2024-06-23 11:10:45 +0900

    Fixed typos

--- Libs/DrawCustomText.lua
+++ Libs/DrawCustomText.lua
@@ -4,7 +4,7 @@ function drawCustomText(cX, cY, text, r, g, b, a)
         if a then
             screen.setColor(r, g, b, a)
         end
-        screen.setColor(r, g, b) 
+        screen.setColor(r, g, b)
     end
     local texts = {}
     for i = 0, #text do
@@ -21,7 +21,7 @@ function drawCustomText(cX, cY, text, r, g, b, a)
         if texts[i] == "E" then
             screen.drawLine(rcX, cY, rcX + 3, cY)
             screen.drawLine(rcX, cY + 4, rcX + 3, cY + 4)
-            screen.drawLine(rcX + 3, cY + 1, rcX+ 3, cY + 4)
+            screen.drawLine(rcX + 3, cY + 1, rcX + 3, cY + 4)
             screen.drawLine(rcX + 1, cY + 2, rcX + 2 + 1, cY + 2)
         end
     end
--- Main.lua
+++ Main.lua
@@ -24,7 +24,6 @@ do
     ---@param simulator Simulator Use simulator:<function>() to set inputs etc.
     ---@param ticks     number Number of ticks since simulator started
     function onLBSimulatorTick(simulator, ticks)
-
         -- touchscreen defaults
         local screenConnection = simulator:getTouchScreen(1)
         simulator:setInputBool(1, screenConnection.isTouched)
@@ -34,11 +33,11 @@ do
         simulator:setInputNumber(4, screenConnection.touchY)
 
         -- NEW! button/slider options from the UI
-        simulator:setInputBool(31, simulator:getIsClicked(1))       -- if button 1 is clicked, provide an ON pulse for input.getBool(31)
-        simulator:setInputNumber(31, simulator:getSlider(1))        -- set input 31 to the value of slider 1
+        simulator:setInputBool(31, simulator:getIsClicked(1))     -- if button 1 is clicked, provide an ON pulse for input.getBool(31)
+        simulator:setInputNumber(31, simulator:getSlider(1))      -- set input 31 to the value of slider 1
 
-        simulator:setInputBool(32, simulator:getIsToggled(2))       -- make button 2 a toggle, for input.getBool(32)
-        simulator:setInputNumber(32, simulator:getSlider(2) * 50)   -- set input 32 to the value from slider 2 * 50
+        simulator:setInputBool(32, simulator:getIsToggled(2))     -- make button 2 a toggle, for input.getBool(32)
+        simulator:setInputNumber(32, simulator:getSlider(2) * 50) -- set input 32 to the value from slider 2 * 50
     end;
 end
 ---@endsection
@@ -64,8 +63,9 @@ function onTick()
     battery = math.floor(input.getNumber(7) * 100)
     ticks = ticks + 1
 
-    winchDown = pressed and inputX >= w / 2 - 5 and inputX <= w / 2 - 5 + 4 and inputY >= 1 - 1 and inputY <= 1 + 4 - 1
     winchUp = pressed and inputX >= w / 2 + 2 and inputX <= w / 2 + 2 + 4 and inputY >= 1 - 1 and inputY <= 1 + 4 - 1
+    winchDown = pressed and inputX >= w / 2 - 5 and inputX <= w / 2 - 5 + 4 and inputY >= 1 - 1 and inputY <= 1 + 4 - 1
+
 
     if winchUp == true then
         output.setBool(1, true)
@@ -115,5 +115,4 @@ function onDraw()
     screen.drawRectF(w / 2 + 1 + 1, 1, 5, 5)
     screen.setColor(255, 255, 255)
     screen.drawText(w / 2 + 1 + 2, 1, "-")
-
 end
